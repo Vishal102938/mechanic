4d401abd3a117d00f4489fcc9f5f66322fd60ca2
Vishal Vishwakarma <vishal2004vishwa@gmail.com>
2026-03-13 10:19:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5173","http://localhost:5174"],
+    origin:["https://mechanic-user.onrender.com","http://localhost:5174"],
     credentials:true
 }))
 
