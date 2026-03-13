a067c508cf0dbbce70f661a8275fe8bcafc06140
Vishal Vishwakarma <vishal2004vishwa@gmail.com>
2026-03-13 10:13:24 +0530

    Update AuthContext.jsx

--- user/src/context/AuthContext.jsx
+++ user/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react'
 export const authDataContext = createContext()
 
 function AuthContext ({children}){
-  let serverUrl = "http://localhost:8000"
+  let serverUrl = "https://mechanic-backend1.onrender.com"
 
   let value = {
     serverUrl
